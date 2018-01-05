211faccc38f79dbd009aa41224966352687c378d
Piotr Lewiński <lewinskip@EMEA.LOCAL>
2018-01-05 11:25:54 +0100

    notes done

--- StickyNotes/js/main.js
+++ StickyNotes/js/main.js
@@ -6,7 +6,9 @@
         onDrag,
         onDragEnd,
         grabPointY,
-        grabPointX;
+        grabPointX,
+        createNote,
+        addNoteBtnEl;
         
 
     onDragStart = function (ev) {
@@ -49,7 +51,31 @@
         grabPointY = null;
     };
     
+    createNote = function () {
+        var stickerEl = document.createElement('div'),
+            barEl = document.createElement('div'),
+            textareaEl = document.createElement('textarea');
+        
+        var transformCSSValue = "translateX(" + Math.random() * 400 + "px) translateY(" + Math.random() * 400 + "px)";
+        
+        stickerEl.style.transform = transformCSSValue;
+        
+        barEl.classList.add('bar');
+        stickerEl.classList.add('sticker');
+        
+        stickerEl.append(barEl);
+        stickerEl.appendChild(textareaEl);
+        
+        stickerEl.addEventListener('mousedown', onDragStart, false);
+        
+        document.body.appendChild(stickerEl);
+    };
+    
+    createNote();
+    
+    addNoteBtnEl = document.querySelector('.addNoteBtn');
+    addNoteBtnEl.addEventListener('click', createNote, false);
     document.addEventListener('mousemove', onDrag, false);
     document.addEventListener('mouseup', onDragEnd, false);
-    document.querySelector('.sticker').addEventListener('mousedown', onDragStart, false);
+    
 })();
